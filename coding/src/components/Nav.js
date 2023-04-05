434da0eaa4c376a5eb04d873dd2af1d1740567e3
aman <amanshrivastav91pp@gmail.com>
2023-04-05 17:43:53 +0530

    made form layout

--- coding/src/components/Nav.js
+++ coding/src/components/Nav.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import "./Nav.css"
 import Login from './Login'
+import { useState } from 'react'
+import { Popup } from 'reactjs-popup';
+
 
 
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
 
     <div className='wrapper'>
       <div className='logo'>
         <img src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" />
       </div>
-
-
-
-
       <div className='nav-items'>
         <ul>
           <li>About</li>
@@ -24,8 +25,14 @@ const Nav = () => {
 
       <div className='sign-up-details'>
         <ul>
-          <li>Sign up</li>
-          {/* <li><Login /></li> */}
+          <li>
+            <button onClick={() => setIsOpen(!isOpen)}>Sign up</button>
+            <Popup open={isOpen} onClose={() => setIsOpen(!isOpen)}>
+              <form>
+                fdsjk
+              </form>
+            </Popup>
+          </li>
         </ul>
       </div>
       <Login />
