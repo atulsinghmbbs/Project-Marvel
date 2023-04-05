8621508372139736707bc02b5be283bd6744fd85
aman <amanshrivastav91pp@gmail.com>
2023-04-05 14:32:32 +0530

    added login functionality

--- coding/src/App.js
+++ coding/src/App.js
@@ -1,38 +1,12 @@
-
-
 import Nav from './components/Nav';
-import LogoutButton from "./components/Logout";
-import LoginButton from "./components/Login";
 import { useEffect } from 'react';
-import { gapi } from "gapi-script";
 
-
-
-
-const clientId = "530760682193-sofqsmesd12559btv57726375g42ljit.apps.googleusercontent.com"
 
 const App = () => {
 
-  useEffect(() => {
-    function start() {
-      gapi.clientId.init({
-        clientId: clientId,
-        scope: ""
-      })
-    }
-    gapi.load("client:auth2", start)
-  })
-
   return (
     <div>
-
       <Nav />
-
-      <LoginButton />
-      <LogoutButton />
-
-
-
     </div>
   )
 }
