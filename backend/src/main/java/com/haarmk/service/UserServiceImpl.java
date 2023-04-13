dfba8ccbbb54ffcbfcc072c0eec5a1d1f865b8c0
Ankit Patel <ankitpatel.akp4.com>
2023-04-13 10:56:33 +0530

    property file changed

--- backend/src/main/java/com/haarmk/service/UserServiceImpl.java
+++ backend/src/main/java/com/haarmk/service/UserServiceImpl.java
@@ -1,24 +1,17 @@
 package com.haarmk.service;
 
-import com.haarmk.feedbackmodel.Feedback;
-import com.haarmk.model.ImageUtils;
+
 import com.haarmk.model.User;
 import com.haarmk.repository.UserRepo;
 import com.haarmk.service.interfaces.UserService;
 
-
-import java.io.IOException;
-
-
 import java.util.Optional;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
+
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -31,20 +24,6 @@ public class UserServiceImpl implements UserService {
     }
 
 	
-    
-    @Override
-	public User addUser(User user , MultipartFile file) {
-    	if(user == null) {
-    		throw new IllegalArgumentException("not a valid argument");
-
-    	}
-    	User saveduser = userRepo.save(user);
-    	try {
-			saveduser.setImagedata(ImageUtils.compressImage(file.getBytes()));
-		} catch (IOException e) {
-			
-			e.printStackTrace();
-		}
 
 	@Override
 	public User addUser(User user) {
@@ -55,9 +34,9 @@ public class UserServiceImpl implements UserService {
 			return userRepo.save(user);
 			
 		}else throw new IllegalArgumentException("User allready exist...");
-
-		
-			
-		return saveduser;
 	}
+
+
+
+
 }
--- backend/src/main/java/com/haarmk/service/interfaces/UserService.java
+++ backend/src/main/java/com/haarmk/service/interfaces/UserService.java
@@ -1,12 +1,11 @@
 package com.haarmk.service.interfaces;
 
-import org.springframework.web.multipart.MultipartFile;
 
 import com.haarmk.model.User;
 
 public interface UserService {
 	
-	User addUser(User user , MultipartFile file);
+	User addUser(User user);
 	
 	User getUserByUsername(String username);
 }
