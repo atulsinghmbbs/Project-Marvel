1070941c1f8556f16e4021e5d311c6be57394d6e
Ankit Patel <ankitpatel.akp4.com>
2023-04-14 12:27:21 +0530

    whois and domain search fixed

--- backend/src/main/java/com/haarmk/controller/DomainComtroller.java
+++ backend/src/main/java/com/haarmk/controller/DomainComtroller.java
@@ -1,9 +1,7 @@
 package com.haarmk.controller;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -35,39 +33,15 @@ public class DomainComtroller {
 	@Autowired private Gson gson;
 //	String baseUrl = "https://api.name.com";
 	String baseUrl = "https://api.dev.name.com";
+	
+	DomainSearchReq domainSearchReq = new DomainSearchReq(1000, null, null);
+	
 	@PostMapping(value= "/check-availability")
 	public ResponseEntity<CheckDomainNameAvailabilityRes> chekDamainNameAvalabillity(@RequestParam String searchTerm) {
-		String url = baseUrl+"/v4/domains:checkAvailability";
-		       
-        HttpEntity<String> requestEntity = new HttpEntity<>(domainNameSuggestion(searchTerm), getDomainHeader());        
-        ResponseEntity<String> responseEntity = this.restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
-        CheckDomainNameAvailabilityRes availabilityRes = gson.fromJson(responseEntity.getBody(), CheckDomainNameAvailabilityRes.class);
-        
-        
-        List<DomainCheckData> suggestions = new ArrayList<>();
-        
-        for(int i = 0; i < availabilityRes.getSuggestions().size(); i++) {
-        	if(availabilityRes.getSuggestions().get(i).getDomainName().equals(searchTerm)) {
-        		availabilityRes.setResult(availabilityRes.getSuggestions().get(i));
-        	}else if (availabilityRes.getSuggestions().get(i).isPurchasable()) {
-				suggestions.add(availabilityRes.getSuggestions().get(i));
-			}
-        }
-        availabilityRes.setSuggestions(suggestions);
-        if(availabilityRes.getSuggestions().isEmpty() && !availabilityRes.getResult().isPurchasable()) {
-        	throw new DomainException("No domain found for: "+searchTerm);
-        }
-        
-        return new ResponseEntity<>(availabilityRes, HttpStatus.OK);
-	}
-	
-	
-	private String domainNameSuggestion(String searchTerm) {
-		CheckDomainNameAvailabilityReq checkDomainNameAvailabilityReq = new CheckDomainNameAvailabilityReq();
-		String[] suggestedTLDs = {".in",".org",".io",".net",".ai",".store",".info",".online"};
-//		String[] suggestedDomainNamePrefix = {"",".in",".org",".io",".net",".ai",".store"};
-//		String[] suggestedDomainNameSuffix = {"info",".tech",".og",".io",".net",".ai",".store"};
+		String urlCheckAvailability = baseUrl+"/v4/domains:checkAvailability";
+		String urlSearch = baseUrl+"/v4/domains:search";
 		
+		CheckDomainNameAvailabilityReq checkDomainNameAvailabilityReq = new CheckDomainNameAvailabilityReq();
 		String[] rootDomain = searchTerm.split("\\.");
 		String domain = rootDomain[0];
 		String defaultTld = ".com";
@@ -77,15 +51,60 @@ public class DomainComtroller {
 		}else {
 			checkDomainNameAvailabilityReq.getDomainNames().add(searchTerm+defaultTld);
 		}
-		System.out.println(defaultTld);
-		for(int i = 0; i < suggestedTLDs.length; i++) {
-			if(! (suggestedTLDs[i]).equals("."+defaultTld)) {
-				checkDomainNameAvailabilityReq.getDomainNames().add(domain + suggestedTLDs[i]);
+//		Check availability
+        HttpEntity<String> requestEntity = new HttpEntity<>(gson.toJson(checkDomainNameAvailabilityReq), getDomainHeader());        
+        ResponseEntity<String> responseEntity = this.restTemplate.exchange(urlCheckAvailability, HttpMethod.POST, requestEntity, String.class);
+        CheckDomainNameAvailabilityRes availabilityRes = gson.fromJson(responseEntity.getBody(), CheckDomainNameAvailabilityRes.class);
+        
+//      search domain
+        domainSearchReq.setKeyword(rootDomain[0]);
+        HttpEntity<String> requestEntitySearch = new HttpEntity<>(gson.toJson(domainSearchReq), getDomainHeader());        
+        ResponseEntity<String> responseEntitySearch = this.restTemplate.exchange(urlSearch, HttpMethod.POST, requestEntitySearch, String.class);
+        CheckDomainNameAvailabilityRes searchRes = gson.fromJson(responseEntitySearch.getBody(), CheckDomainNameAvailabilityRes.class);
+        
+        
+        List<DomainCheckData> suggestions = new ArrayList<>();
+        
+        for(int i = 0; i < searchRes.getSuggestions().size(); i++) {
+        	if (searchRes.getSuggestions().get(i).isPurchasable()) {
+				suggestions.add(searchRes.getSuggestions().get(i));
 			}
-			
-		}
-		return gson.toJson(checkDomainNameAvailabilityReq);
+        }
+        System.out.println(availabilityRes);
+        searchRes.setResult(availabilityRes.getSuggestions().get(0));
+        searchRes.setSuggestions(suggestions);
+        if(availabilityRes.getSuggestions().isEmpty() && !availabilityRes.getResult().isPurchasable()) {
+        	throw new DomainException("No domain found for: "+searchTerm);
+        }
+        
+        return new ResponseEntity<>(searchRes, HttpStatus.OK);
 	}
+	
+	
+//	private String domainNameSuggestion(String searchTerm) {
+//		CheckDomainNameAvailabilityReq checkDomainNameAvailabilityReq = new CheckDomainNameAvailabilityReq();
+//		String[] suggestedTLDs = {".in",".org",".io",".net",".ai",".store",".info",".online"};
+////		String[] suggestedDomainNamePrefix = {"",".in",".org",".io",".net",".ai",".store"};
+////		String[] suggestedDomainNameSuffix = {"info",".tech",".og",".io",".net",".ai",".store"};
+//		
+//		String[] rootDomain = searchTerm.split("\\.");
+//		String domain = rootDomain[0];
+//		String defaultTld = ".com";
+//		if(rootDomain.length == 2) {
+//			checkDomainNameAvailabilityReq.getDomainNames().add(searchTerm);
+//			defaultTld = rootDomain[1];
+//		}else {
+//			checkDomainNameAvailabilityReq.getDomainNames().add(searchTerm+defaultTld);
+//		}
+//		System.out.println(defaultTld);
+//		for(int i = 0; i < suggestedTLDs.length; i++) {
+//			if(! (suggestedTLDs[i]).equals("."+defaultTld)) {
+//				checkDomainNameAvailabilityReq.getDomainNames().add(domain + suggestedTLDs[i]);
+//			}
+//			
+//		}
+//		return gson.toJson(checkDomainNameAvailabilityReq);
+//	}
 	
 	private HttpHeaders getDomainHeader() {
 		HttpHeaders headers = new HttpHeaders();
@@ -96,43 +115,6 @@ public class DomainComtroller {
 	}
 	
 	
-	
-	
-	
-//	@PostMapping(value= "/search")
-//	public ResponseEntity<CheckDomainNameAvailabilityRes> searchDomain(@RequestParam String searchTerm) {
-//		String url = baseUrl+"/v4/domains:search";
-//		List<String> tlds = Arrays.asList("com","org","in");
-//		String[] rootDomain = searchTerm.split("\\.");
-//		String domain = rootDomain[0];
-//		String defaultTld = null;
-//		if(rootDomain.length == 2) {
-//			defaultTld = rootDomain[1];
-//			tlds.add(defaultTld);
-//			
-//		}
-//		
-//		DomainSearchReq domainSearchReq = new DomainSearchReq(1000,domain,tlds);
-//		gson.toJson(domainSearchReq);       
-//        HttpEntity<String> requestEntity = new HttpEntity<>(gson.toJson(domainSearchReq), getDomainHeader());        
-//        ResponseEntity<String> responseEntity = this.restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
-//        CheckDomainNameAvailabilityRes availabilityRes = gson.fromJson(responseEntity.getBody(), CheckDomainNameAvailabilityRes.class);
-//        
-        
-//        for(int i = 0; i < availabilityRes.getSuggestions().size(); i++) {
-//        	if(availabilityRes.getSuggestions().get(i).getDomainName().equals(searchTerm)) {
-//        		availabilityRes.setResult(availabilityRes.getSuggestions().get(i));
-//        	}else if (availabilityRes.getSuggestions().get(i).isPurchasable()) {
-//				suggestions.add(availabilityRes.getSuggestions().get(i));
-//			}
-//        }
-//        availabilityRes.setSuggestions(suggestions);
-//        if(availabilityRes.getSuggestions().isEmpty() && !availabilityRes.getResult().isPurchasable()) {
-//        	throw new DomainException("No domain found for: "+searchTerm);
-//        }
-//        
-//        return new ResponseEntity<>(availabilityRes, HttpStatus.OK);
-//	}
 	
 	
 	
